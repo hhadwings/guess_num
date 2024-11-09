cb285499f965bdc7c4af562e708ed03d9f3e9ecb
N-Soni0 <anton.stecyura.pr1@gmail.com>
2024-11-09 20:35:50 +0200

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const minNum = 0;
@@ -9,27 +9,25 @@ const maxNum = 30;
 
 let randomNumber = getRandomInt(minNum, maxNum);
 
-const inputElement = document.querySelector('input')
-const submitBtn = document.querySelector('.ok')
+const inputElement = document.querySelector('input');
+const submitBtn = document.querySelector('.ok');
 
 submitBtn.addEventListener('click', (e) => {
-    const guess = Number(inputElement.value);
+	const guess = Number(inputElement.value);
 
-    if (Number.isNaN(guess)) {
-        alert("Введіть число!")
-        return;
-    }
-    
-    if (guess < minNum || guess > maxNum) {
-        alert(`Число повинно бути від ${minNum} до ${maxNum}`)
-    }
+	if (Number.isNaN(guess)) {
+		alert('Введіть число!');
+		return;
+	}
 
-    if (guess === randomNumber) {
-        alert("Ви вгадали")
-    } else {
-        alert("Ви не вгадали (лошара)")
-    }
-    
-    randomNumber = getRandomInt(minNum, maxNum);
-    inputElement.value = "";
-})
+	if (guess < minNum || guess > maxNum) {
+		alert(`Число повинно бути від ${minNum} до ${maxNum}`);
+	} else if (guess === randomNumber) {
+		alert('Ви вгадали');
+	} else {
+		alert('Ви не вгадали (лошара)');
+	}
+
+	randomNumber = getRandomInt(minNum, maxNum);
+	inputElement.value = '';
+});
