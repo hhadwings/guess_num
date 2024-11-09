0aa5b0e889dac3061600e30cf6df1bea650f1851
N-Soni0 <anton.stecyura.pr1@gmail.com>
2024-11-09 20:11:03 +0200

    Add functionality

--- index.js
+++ index.js
@@ -4,4 +4,31 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// текстове поле
+let randomNumber = getRandomInt(1, 100);
+
+const inputElement = document.querySelector('input')
+const submitBtn = document.querySelector('.ok')
+const restartBtn = document.querySelector('.restart')
+
+submitBtn.addEventListener('click', (e) => {
+    const guess = Number(inputElement.value);
+
+    if (Number.isNaN(guess)) {
+        alert("Введіть число!")
+        return;
+    }
+
+    if (guess === randomNumber) {
+        alert("Ви вгадали")
+    } else if (guess > randomNumber) {
+        alert("Ви не вгадали. Ваше число надто велике")
+    } else if (guess < randomNumber) {
+        alert("Ви не вгадали. Ваше число надто мале")
+    }
+})
+
+
+restartBtn.addEventListener('click', (e) => {
+    randomNumber = getRandomInt();
+    inputElement.value = "";
+})
